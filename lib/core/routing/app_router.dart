486962551ebb7b1fcc35e9972ab35ea3e95e2cea
AbdelrahmanHosni626 <abdohosny626@gmail.com>
2024-03-11 14:12:34 +0200

    - Create Books Model - Create Featured Books cubit and States - Add Bloc Observer

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,9 +1,8 @@
-
 import 'package:bookly_app/core/routing/routes.dart';
-import 'package:bookly_app/features/book_details/book_details_screen.dart';
 import 'package:bookly_app/features/search/ui/search_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../../features/book_details/ui/book_details_screen.dart';
 import '../../features/home/ui/home_screen.dart';
 
 class AppRouter {
---
+++ lib/features/home/logic/best_seller_cubit/best_seller_books_states.dart
@@ -0,0 +1,18 @@
+import '../../data/models/book_model/book_model.dart';
+abstract class BestSellerBooksStates {}
+
+class BestSellerBooksInitialState extends BestSellerBooksStates {}
+
+class BestSellerBooksLoadingState extends BestSellerBooksStates {}
+
+class BestSellerBooksSuccessState extends BestSellerBooksStates {
+  final List<BookModel> books;
+
+  BestSellerBooksSuccessState(this.books);
+}
+
+class BestSellerBooksErrorState extends BestSellerBooksStates {
+  final String error;
+
+  BestSellerBooksErrorState(this.error);
+}
