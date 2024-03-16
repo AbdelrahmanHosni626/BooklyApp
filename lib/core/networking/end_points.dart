da588ff205835d5c0b42cc7a255b0f914b6a9a59
AbdelrahmanHosni626 <abdohosny626@gmail.com>
2024-03-16 13:32:41 +0200

    - Create Similar Books Cubit - Add Similar Books End Point

--- lib/core/networking/end_points.dart
+++ lib/core/networking/end_points.dart
@@ -1,2 +1,3 @@
 const bestSellerBooksEndPoint = 'volumes?q=subject:programming&Filtering=free-ebooks&sorting=newest';
 const featuredBooksEndPoint = 'volumes?q=subject:programming&Filtering=free-ebooks';
+const similarBooksEndPoint = 'volumes?q=subject:programming&Filtering=free-ebooks&sorting=relevance';
---
+++ lib/features/home/logic/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/features/home/logic/similar_books_cubit/similar_books_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../data/repos/home_repo.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksStates> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitialState());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilarBooks() async {
+    emit(SimilarBooksLoadingState());
+    var result  = await homeRepo.fetchSimilarBooks();
+    result.fold(
+          (failure) {
+        emit(SimilarBooksErrorState(failure.errMessage));
+      },
+          (books) {
+        emit(
+          SimilarBooksSuccessState(books),
+        );
+      },
+    );
+  }
+}
